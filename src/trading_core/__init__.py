fa0998b7c1d4902c65bcd45a7967a207018fa49c
doomit <zqsand@gmail.com>
2026-08-31 10:06:08 -0700

    export paper exit lifecycle helper

--- src/trading_core/__init__.py
+++ src/trading_core/__init__.py
@@ -1,1 +1,8 @@
 """Public, credential-free trading domain library."""
+
+from . import paper_execution as paper_execution
+from .paper_exit import close_open_position
+
+paper_execution.close_open_position = close_open_position
+
+__all__ = ["close_open_position", "paper_execution"]
