9a3e0d9988905a320846ec6630f0c2131b7af34a
doomit <zqsand@gmail.com>
2026-09-03 12:08:21 -0700

    Gate9: bind deterministic pending LIMIT replacement

--- src/trading_core/__init__.py
+++ src/trading_core/__init__.py
@@ -3,10 +3,12 @@
 from . import paper_execution as paper_execution
 from .paper_exit import close_open_position
 from .pending_cancel import cancel_pending
+from .pending_replace import replace_pending_limit
 from .pending_stop import process_pending_stop
 
 paper_execution.close_open_position = close_open_position
 paper_execution.DeterministicPaperBroker.cancel_pending = cancel_pending
+paper_execution.DeterministicPaperBroker.replace_pending_limit = replace_pending_limit
 paper_execution.DeterministicPaperBroker.process_pending_stop = process_pending_stop
 
 __all__ = ["close_open_position", "paper_execution"]
