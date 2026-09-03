53bd0e3bfc1aa632cf4bd30e38cd59f979245a88
doomit <zqsand@gmail.com>
2026-09-03 12:06:47 -0700

    Gate9: bind deterministic pending cancellation

--- src/trading_core/__init__.py
+++ src/trading_core/__init__.py
@@ -2,9 +2,11 @@
 
 from . import paper_execution as paper_execution
 from .paper_exit import close_open_position
+from .pending_cancel import cancel_pending
 from .pending_stop import process_pending_stop
 
 paper_execution.close_open_position = close_open_position
+paper_execution.DeterministicPaperBroker.cancel_pending = cancel_pending
 paper_execution.DeterministicPaperBroker.process_pending_stop = process_pending_stop
 
 __all__ = ["close_open_position", "paper_execution"]
