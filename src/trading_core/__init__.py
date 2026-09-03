d8663a897ae2bd7548d29152a2116b78e5889542
doomit <zqsand@gmail.com>
2026-09-03 12:03:15 -0700

    Gate9: bind pending STOP processor

--- src/trading_core/__init__.py
+++ src/trading_core/__init__.py
@@ -2,7 +2,9 @@
 
 from . import paper_execution as paper_execution
 from .paper_exit import close_open_position
+from .pending_stop import process_pending_stop
 
 paper_execution.close_open_position = close_open_position
+paper_execution.DeterministicPaperBroker.process_pending_stop = process_pending_stop
 
 __all__ = ["close_open_position", "paper_execution"]
