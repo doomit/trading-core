4d42bc829cc99ad77f6861bf614b0a292fbb01a6
doomit <zqsand@gmail.com>
2026-09-03 12:18:31 -0700

    feat: wire pending session-close cancellation

--- src/trading_core/__init__.py
+++ src/trading_core/__init__.py
@@ -4,11 +4,13 @@ from . import paper_execution as paper_execution
 from .paper_exit import close_open_position
 from .pending_cancel import cancel_pending
 from .pending_replace import replace_pending_limit
+from .pending_session_close import cancel_pending_for_session_close
 from .pending_stop import process_pending_stop
 
 paper_execution.close_open_position = close_open_position
 paper_execution.DeterministicPaperBroker.cancel_pending = cancel_pending
 paper_execution.DeterministicPaperBroker.replace_pending_limit = replace_pending_limit
+paper_execution.DeterministicPaperBroker.cancel_pending_for_session_close = cancel_pending_for_session_close
 paper_execution.DeterministicPaperBroker.process_pending_stop = process_pending_stop
 
 __all__ = ["close_open_position", "paper_execution"]
