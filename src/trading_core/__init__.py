24895e618acd6100d1d23917b69acb16582f2ddc
doomit <zqsand@gmail.com>
2026-09-03 12:21:53 -0700

    feat: expose exposure reservation release

--- src/trading_core/__init__.py
+++ src/trading_core/__init__.py
@@ -2,12 +2,14 @@
 
 from . import paper_execution as paper_execution
 from . import paper_exit as paper_exit
+from . import risk_admission as risk_admission
 from .paper_exit import close_open_position
 from .paper_session_flatten import flatten_open_position_at_session_close
 from .pending_cancel import cancel_pending
 from .pending_replace import replace_pending_limit
 from .pending_session_close import cancel_pending_for_session_close
 from .pending_stop import process_pending_stop
+from .risk_admission_release import release_exposure
 
 paper_execution.close_open_position = close_open_position
 paper_execution.DeterministicPaperBroker.cancel_pending = cancel_pending
@@ -15,5 +17,6 @@ paper_execution.DeterministicPaperBroker.replace_pending_limit = replace_pending
 paper_execution.DeterministicPaperBroker.cancel_pending_for_session_close = cancel_pending_for_session_close
 paper_execution.DeterministicPaperBroker.process_pending_stop = process_pending_stop
 paper_exit.flatten_open_position_at_session_close = flatten_open_position_at_session_close
+risk_admission.release_exposure = release_exposure
 
 __all__ = ["close_open_position", "flatten_open_position_at_session_close", "paper_execution"]
